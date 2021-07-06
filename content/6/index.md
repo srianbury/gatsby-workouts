c4e27cb8806205058f9531bd30fdad6af97004aa
Brian Sunbury <bsunbury29@gmail.com>
2021-07-06 16:51:16 -0700

    link and tags

--- content/6/index.md
+++ content/6/index.md
@@ -4,14 +4,14 @@ channel: Men's Health
 date: "2021-07-06"
 banner: "./images/banner.jpg"
 url: https://www.youtube.com/watch?v=Kuv0xThzxrU
-tags: abs,at home
+tags: conditioning,at home,chest,biceps,legs,triceps,shoulders,abs
 ---
 
 ## Warm Up
 1. Bear Crawls - 20s On / 10s Off - 10 rounds
 2. Body Weight Squats - 20s On / 10s Off - 10 rounds
 
-## Workout 
+## Workout
 ### 8 Reps Each - Repeat Circuit for 3 Rounds
 1. Weighted Burpee
 2. Curl and Press
--- src/pages/index.js
+++ src/pages/index.js
@@ -3,39 +3,41 @@ import { Link, graphql } from "gatsby";
 import Layout from "../components/layout";
 import { GatsbyImage, getImage } from "gatsby-plugin-image";
 
+function alphaSort(a, b) {
+  return a.toLowerCase() > b.toLowerCase
+    ? 1
+    : a.toLowerCase() < b.toLowerCase()
+    ? -1
+    : 0;
+}
+
 const Home = ({ data }) => {
   return (
     <Layout title="Home">
       <div className="mx-auto max-w-screen-md">
         {data.allMarkdownRemark.edges.map(({ node }) => (
-          <div key={node.id}>
-            <div className="max-w-md mx-auto bg-white rounded-xl shadow-md overflow-hidden md:max-w-2xl mb-4">
-              <div className="md:flex">
+          <div className="max-w-md mx-auto bg-white rounded-xl shadow-md overflow-hidden md:max-w-2xl mb-4">
+            <div className="md:flex">
+              <Link key={node.id} to={node.fields.slug}>
                 <div className="md:flex-shrink-0">
-                  <Link to={node.fields.slug}>
-                    <GatsbyImage
-                      className="h-64 w-full object-cover md:h-full md:w-64"
-                      image={getImage(node.frontmatter.banner)}
-                      alt="banner"
-                    />
-                  </Link>
+                  <GatsbyImage
+                    className="h-64 w-full object-cover md:h-full md:w-64"
+                    image={getImage(node.frontmatter.banner)}
+                    alt="banner"
+                  />
                 </div>
                 <div className="px-4 py-2">
-                  <Link to={node.fields.slug} className="hover:underline">
-                    <h2>{node.frontmatter.title}</h2>
-                  </Link>
+                  <h2 className="hover:underline">{node.frontmatter.title}</h2>
                   <div>{node.frontmatter.channel}</div>
                   <p className="mt-2 text-gray-500">
-                    {node.frontmatter.tags.split(",").join(", ")}
+                    {node.frontmatter.tags
+                      .split(",")
+                      .map(val => val.trim())
+                      .sort(alphaSort)
+                      .join(", ")}
                   </p>
-                  <Link
-                    to={node.fields.slug}
-                    className="text-purple-900 hover:underline"
-                  >
-                    View
-                  </Link>
                 </div>
-              </div>
+              </Link>
             </div>
           </div>
         ))}
