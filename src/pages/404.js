8c106f4be58806461c82b62a40ada878bb1c2932
Brian Sunbury <bsunbury29@gmail.com>
2021-07-09 15:44:15 -0700

    404 location fixed

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,7 +1,7 @@
 import React from "react";
 import Layout from "../components/layout";
 
-const NotFoundPage = location => (
+const NotFoundPage = ({ location }) => (
   <Layout title="404: Not found" url={location.href}>
     <h1>404: Not Found</h1>
   </Layout>
